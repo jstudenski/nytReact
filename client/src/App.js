10d70c4c5b532a0cc9b196c7f1db1d0698d0b052
jstudenski <jstudenski@gmail.com>
2018-04-19 16:03:10 -0500

    created results card

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Section from './components/Section';
 import Form from './components/Form';
-
-
+import Results from './components/Results';
 
 class App extends Component {
   render() {
@@ -13,8 +12,8 @@ class App extends Component {
           <h1 className="App-title">New York Times Search</h1>
         </header>
 
-        <Section title="Search"><Form/></Section>
-        <Section title="Results"></Section>
+        <Section title="Search"><Form /></Section>,
+        <Section title="Results"><Results /></Section>,
         <Section title="Saved Articles"></Section>
         {/* <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
