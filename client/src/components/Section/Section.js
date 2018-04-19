e0be044a4f5eaa392dbd97dff83d0105d077f408
jstudenski <jstudenski@gmail.com>
2018-04-19 15:54:10 -0500

    added axios

--- client/src/components/Section/Section.js
+++ client/src/components/Section/Section.js
@@ -1,57 +1,13 @@
 import React from 'react';
 import './Section.css';
 
-
-// const Section = React.createClass({
-//   render: function() {
-//     return (
-//       <div className="wrapper">
-//         before
-//           {this.props.children}
-//         after
-//       </div>
-//     );
-//   }
-// });
-
 const Section = props => (
 
   <div className="card">
-      <h1>{props.title}</h1>
-      {props.children}
+    <h1>{props.title}</h1>
+    {props.children}
   </div>
 
-// var CheckLink = React.createClass({
-//   render: function() {
-//     // This takes any props passed to CheckLink and copies them to <a>
-//     return 
-//   }
-// });
-
-  // <h1>{this.props.title}</h1>
-  // {this.props.children}
-
-
-  // <div className="card">
-
-    /* <div className="content">
-      <ul>
-        <li>
-          <strong>Name:</strong> {props.name}
-        </li>
-        <li>
-          <strong>Occupation:</strong> {props.occupation}
-        </li>
-        <li>
-          <strong>Location:</strong> {props.location}
-        </li>
-      </ul>
-    </div> */
-    /* <span onClick={() => props.removeFriend(props.id)} className="remove">
-      𝘅
-    </span> */
-  // </div>
 );
 
 export default Section;
-//export default Wrapper;
