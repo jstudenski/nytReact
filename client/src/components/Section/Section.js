68db151629824e8e99d5ce8d4b224cd93a159a55
jstudenski <jstudenski@gmail.com>
2018-04-20 14:43:47 -0500

    section lint

--- client/src/components/Section/Section.js
+++ client/src/components/Section/Section.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Section.css';
 
 const Section = props => (
-
   <div className="card">
     <h1>{props.title}</h1>
     {props.children}
   </div>
-
 );
+
+Section.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node,
+};
+
+Section.defaultProps = {
+  children: 'coming soon..',
+};
 
 export default Section;
